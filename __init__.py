cb413e204e8a33c1511da90bdb0390ba7cb99cda
Noah Wahl <noah.ares@yahoo.de>
2020-09-24 12:50:29 +0200

    Working skill

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from mycroft import MycroftSkill, intent_file_handler
+import mailbox
+from time import sleep
 
+mail_dirs = ['~/.mail/yahoo/Inbox/', '~/.mail/kit/INBOX/']
 
 class ReadMail(MycroftSkill):
     def __init__(self):
@@ -7,9 +10,16 @@ class ReadMail(MycroftSkill):
 
     @intent_file_handler('mail.read.intent')
     def handle_mail_read(self, message):
-        self.speak_dialog('mail.read')
+        for dir in mail_dirs:
+            for message in mailbox.Maildir(dir):
+                if not "S" in message.get_flags():
+                    self.speak(message['subject'])
+                    self.speak('from')
+                    self.speak(message['from'])
+                    sleep(2)
 
+def stop(self):
+    pass
 
 def create_skill():
     return ReadMail()
-
